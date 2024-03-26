f79f069956edc9f4583471673314925ed31d7e59
valteci <valtecijunior@gmail.com>
2024-03-25 21:52:08 -0300

    fim quebra de linha

--- main.py
+++ main.py
@@ -3,40 +3,7 @@
 nome_arquivo_entrada = 'Test.cmp'
 nome_arquivo_saida = 'Test1.cmp'
 
-
-""" def remover_comentario_linha(txt: str):        
-    achou_primeira_barra = False
-    achou_segunda_barra = False
-    achou_fim_linha = False
-    mapeamento = []
-
-    indice = 0
-
-    for caractere in txt:
-        match caractere:
-            case r'/':
-                if achou_primeira_barra == True:
-                    achou_segunda_barra = True
-                else:
-                    achou_primeira_barra = True
-                    
-
-                
-            case _:
-                
-                if achou_segunda_barra and achou_segunda_barra:
-                    pass
-
-                else:
-                    achou_fim_linha = False
-                    achou_primeira_barra = False
-                    achou_segunda_barra = False
-
-        
-        indice += 1
-
- """
-        
+  
 
 def remover_comentario_linha(txt: str) -> str:
     lista_arquivo = list(txt)
@@ -54,7 +21,7 @@ def remover_comentario_multilinhas(txt: str) -> str:
 
     while txt.find('/*') != -1:
         inicio = txt.find('/*')
-        fim = txt.find("*\\", inicio)
+        fim = txt.find("*/", inicio)
 
         if fim == -1:
             lista_txt[inicio :] = []
@@ -65,30 +32,51 @@ def remover_comentario_multilinhas(txt: str) -> str:
 
     return txt
 
-
 def carregar_arquivo() -> str:
     with open(nome_arquivo_entrada) as arquivo:
         txt_arq = arquivo.read()
         
     return txt_arq
-            
 
 def salvar_arquivo(txt: str) -> None:
     with open(nome_arquivo_saida, 'w') as salvar:
             salvar.write(txt)
 
+def remover_quebras_de_linha(txt: str) -> str:
+    achou_quebra_linha = False
+    lista_txt = list(txt)
+    novo_texto = []
+
+    for caractere in lista_txt:
+        if caractere == '\n':
+            if achou_quebra_linha == True:
+                pass
+            else:
+                achou_quebra_linha = True
+                novo_texto.append(caractere)
+        else:
+            novo_texto.append(caractere)
+            achou_quebra_linha = False
 
 
-    
+    while novo_texto[0] == '\n':
+        novo_texto.remove('\n') # remove a primeira ocorrência de quebra de linha (\n)
+
+    return ''.join(novo_texto)
+
  
 arquivo_txt = carregar_arquivo()
 texto = remover_comentario_linha(arquivo_txt)
 texto_sem_comentarios = remover_comentario_multilinhas(texto)
-print(texto_sem_comentarios)
-#salvar_arquivo(texto_sem_comentarios)
+texto_semifinal = remover_quebras_de_linha(texto_sem_comentarios)
+print(texto_semifinal)
+salvar_arquivo(texto_semifinal)
+
+# remover quebras de linha
 
 
-# remover comentários de bloco
+
+    
 
 
 
